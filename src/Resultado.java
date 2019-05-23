409417fe6e9da8eebff03ebf81bab2e4d8862cdd
Lucas Vinicius Cardoso Moro <201919060154@lab01-l56.dominio.facom.ufms.br>
2019-05-23 11:26:36 -0400

    Atualizacao classe Resultado. Criacao metodo verificarVetor

--- src/Resultado.java
+++ src/Resultado.java
@@ -34,4 +34,14 @@ public class Resultado {
     public ValorAvaliacao getAvaliacaoPorMedida(String medida){
     
     }
+
+    public boolean verificarVetor(){
+	    for(int i = 0; i < ValorAvalicao.length; i++){
+		    if(ValorAvaliacao[i] == null){
+			    return false;
+		    }
+	    }
+
+	    return true;
+    }
 }
