ec6e75d43d8b3ad7d779750fdc0a05d1852f75d9
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2019-05-23 14:55:42 -0400

    Criando metodos auxiliares de validacao de strings

--- src/aux/Validador.java
+++ src/aux/Validador.java
@@ -2,4 +2,34 @@ package aux;
 
 public class Validador {
 	
+	// STRINGS - Metodos auxiliares para validacao de Strings
+
+	// Descricao: Verifica se uma string e valida
+    private boolean verificaStringValida(String str) {
+        if (!this.verificaStringVazia(str) && !this.verificaObjetoNulo(str)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+	// Descricao: Verifica se uma string nao possui nenhum caractere alem do espaco
+    private boolean verificaStringVazia(String str) {
+        if (str.replaceAll(" ", "").equals("")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // OBJETOS - Metodos auxiliares para validacao de objetos
+
+    // Descricao: Verifica se um objeto e nulo
+    private boolean verificaObjetoNulo(Object obj) {
+        if (obj == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
