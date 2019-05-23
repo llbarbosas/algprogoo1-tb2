97ff6aac1776fef0af6b9ece48e587ea24065046
LucasMoro01 <lucasmoro_07@outlook.com>
2019-05-23 14:49:03 -0400

    Atualizacao métodos da classe Resultado

--- src/Resultado.java
+++ src/Resultado.java
@@ -8,11 +8,11 @@ public class Resultado {
     }
 
     public Algoritmo getAlgoritmo(){
-    
+   	return Algoritmo; 
     }
 
     public Dataset getDataset(){
-    
+    	
     }
 
     public String getNomeDoAlgoritmo(){
@@ -22,26 +22,39 @@ public class Resultado {
     public String getNomeDoDataset(){
     
     }
-
-    public boolean addAvaliacao(ValorAvaliacao avaliacao){
     
+    public boolean addAvaliacao(ValorAvaliacao avaliacao){
+	//Validar se o vetor avaliacao está cheio, se o vetor estiver cheio return false
+	//Validar se ja possui uma medida de avaliacao igual, se caso possuir return false.
+	//
+    	if(){
+		return false;
+	}else
+		// System.out.println("Impossivel adicionar esta avaliacao");
+		return true;
     }
+	
 
     public ValorAvaliacao[] getAvaliacoes(){
-    
+    	return avaliacao;
     }
 
     public ValorAvaliacao getAvaliacaoPorMedida(String medida){
-    
+    	for(int i = 0; i < avaliacoes.length; i++){
+		if(avaliacoes[i].equalsIgnoreCase(medida){
+			return avaliacao[i];
+		}else
+			return null;
+
     }
 
-    private  boolean verificarVetor(){
-	    for(int i = 0; i < ValorAvalicao.length; i++){
+   /* private int verificarVetorCheio(){
+	    for(int i = 0; i < this.avalicao.length; i++){
 		    if(ValorAvaliacao[i] == null){
-			    return false;
+			    return i;
 		    }
 	    }
 
-	    return true;
-    }
+	    return -1;//Está cheio
+    }*/
 }
