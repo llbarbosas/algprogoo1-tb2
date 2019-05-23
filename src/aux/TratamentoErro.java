06fc5fd1331fa31316cd53a2bf107595be034a2a
Lucas Lima <llbarbosasc@gmail.com>
2019-05-23 14:42:38 -0400

    Adicionando função de tratamento de erro

--- src/aux/TratamentoErro.java
+++ src/aux/TratamentoErro.java
@@ -1,5 +1,12 @@
 package aux;
 
 public class TratamentoErro {
-	
+	private static final String RESET = "\u001B[0m";
+	private static final String AMARELO = "\u001B[33m";
+	private static final String VERMELHO = "\u001B[31m";
+
+	public void erro(String mensagem){
+		System.out.printf("%s[ERRO] %s%s\n", VERMELHO, mensagem, RESET);
+		System.exit(0);
+	}	
 }
