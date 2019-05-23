a6c2244dfab58ec8291be30d8429f9e1b43cf358
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2019-05-23 15:03:03 -0400

    Definindo metodos como publicos e estaticos

--- src/aux/Validador.java
+++ src/aux/Validador.java
@@ -5,7 +5,7 @@ public class Validador {
 	// STRINGS - Metodos auxiliares para validacao de Strings
 
 	// Descricao: Verifica se uma string e valida
-    private boolean verificaStringValida(String str) {
+    public static boolean verificaStringValida(String str) {
         if (!this.verificaStringVazia(str) && !this.verificaObjetoNulo(str)) {
             return true;
         } else {
@@ -14,7 +14,7 @@ public class Validador {
     }
 
 	// Descricao: Verifica se uma string nao possui nenhum caractere alem do espaco
-    private boolean verificaStringVazia(String str) {
+    public static boolean verificaStringVazia(String str) {
         if (str.replaceAll(" ", "").equals("")) {
             return true;
         } else {
@@ -25,7 +25,7 @@ public class Validador {
     // OBJETOS - Metodos auxiliares para validacao de objetos
 
     // Descricao: Verifica se um objeto e nulo
-    private boolean verificaObjetoNulo(Object obj) {
+    public static boolean verificaObjetoNulo(Object obj) {
         if (obj == null) {
             return true;
         } else {
