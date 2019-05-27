bfa5677bf75ca272ade4c3edc675fb236461dde6
Lucas Vinicius Cardoso Moro <201919060154@lab01-l55.dominio.facom.ufms.br>
2019-05-27 10:09:50 -0400

    Atualizacao na classe Resultado

--- src/Resultado.java
+++ src/Resultado.java
@@ -8,7 +8,8 @@ public class Resultado {
     private ValorAvaliacao[] avaliacao;
 
     public Resultado(Algoritmo algoritmo, Dataset dataset){
-    
+        this.algoritmo = algoritmo;
+        this.dataset = dataset;
     }
 
     public Algoritmo getAlgoritmo(){
@@ -16,43 +17,43 @@ public class Resultado {
     }
 
     public Dataset getDataset(){
-    	
+    	return dataset;
     }
 
     public String getNomeDoAlgoritmo(){
-    
+        return algoritmo.getnome();
     }
 
     public String getNomeDoDataset(){
-    
+        return dataset.getNome();
     }
     
     public boolean addAvaliacao(ValorAvaliacao avaliacao){
 	//Validar se o vetor avaliacao está cheio, se o vetor estiver cheio return false
-    //Validar se ja possui uma medida de avaliacao igual, se caso possuir return false.
-    
-    	if(Validador.verificaArrayCheio(this.avaliacao)){ //Verificar se o vetor está cheio
+        //Validar se ja possui uma medida de avaliacao igual, se caso possuir return false.
+    	if(!Validador.verificaArrayCheio(this.avaliacao)){ //Verificar se o vetor está cheio
             for(int i = 0; i < this.avaliacao.length; i++){
-                if(avaliacao[i].equalsIgnoreCase(avaliacao[0]) && avaliacao[i].equalsIgnoreCase(avaliacao[1]))
+                if(this.avaliacao[i] != null){
+                    this.avaliacao[i] = avaliacao;
                     return true;
+                }
             }
         }else
-		TratamentoErro.erroSimples(mensagem);
-		return false;
-    }
-	
-    
+            TratamentoErro.erroSimples(mensagem);
+            return false;// Caso o array de avaliacao estiver cheio, então retorna false.
+    }            
+ 
     public ValorAvaliacao[] getAvaliacoes(){
     	return avaliacao;
     }
 
     public ValorAvaliacao getAvaliacaoPorMedida(String medida){
     	for(int i = 0; i < avaliacoes.length; i++){
-		    if(avaliacoes[i].equalsIgnoreCase(medida){
-			    return avaliacao[i];
-		    }else
+            if(avaliacoes[i].equalsIgnoreCase(medida){
+                return avaliacao[i];
+            }else
                 return null;
         }
-
+    
     }
 }
