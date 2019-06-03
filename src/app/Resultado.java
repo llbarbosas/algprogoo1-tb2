5f56a9fd89e2866f9483d112183c391142e6cde4
LucasMoro01 <lucasmoro_07@outlook.com>
2019-06-03 15:52:31 -0300

    Revisão e comentários da classe Resultado

--- src/app/Resultado.java
+++ src/app/Resultado.java
@@ -1,13 +1,17 @@
 package app;
-import util.Validador;
-import util.TratamentoErro;
+import util.Validador;// Import da Classe Validador
+import util.TratamentoErro;// Import da Classe TratamentoErro
 
 public class Resultado {
+    
+    //Declaração de atributos
     private Algoritmo algoritmo;
     private Dataset dataset;
     private ValorAvaliacao[] avaliacao;
 
+    // Método construtor
     public Resultado(Algoritmo algoritmo, Dataset dataset){
+        
         // Verifica se o algoritmo não é nulo
         if(Validador.verificaObjetoValido(algoritmo)) {
             this.algoritmo = algoritmo;
@@ -22,34 +26,36 @@ public class Resultado {
             TratamentoErro.erro("O objeto informado não pode ser nulo.");
         }
 
+        //Instanciação do array avaliacao
         this.avaliacao = new ValorAvaliacao[3];
     }
 
+    //Método que retorna o algoritmo usado.
     public Algoritmo getAlgoritmo(){
    	    return this.algoritmo; 
     }
-
+    //Método que retorna o dataset usado.
     public Dataset getDataset(){
     	return this.dataset;
     }
-
+    //Método que retorna o nome do algoritmo usado.
     public String getNomeDoAlgoritmo(){
         return this.algoritmo.getNome();
     }
-
+    //Método que retorna o nome do dataset usado.
     public String getNomeDoDataset(){
         return this.dataset.getNome();
     }
-    
+    //Método para adicinar uma avaliacao no array de avaliações.
     public boolean addAvaliacao(ValorAvaliacao avaliacao){
-    	if(!Validador.verificaArrayCheio(this.avaliacao)){ // Verifica se o vetor está cheio
+    	if(!Validador.verificaArrayCheio(this.avaliacao)){ // Verifica se o vetor está cheio.
             if(this.verificaMedidaAvaliacaoIgual(avaliacao)){ // Verifica se possui medida de avaliação igual.
                 System.out.println("Impossivel adicionar avaliacao, pois ja existe esta medida de avaliacao");
                 return false;
             }
 
             int posicaoDeInsercao = this.verificaPosicaoDeInsercao();// Auxiliar para receber qual a primeira posição vazia do vetor.
-            if (posicaoDeInsercao > -1){
+            if (posicaoDeInsercao > -1){// Verifica se a posição vazia do array é valida.
                 this.avaliacao[posicaoDeInsercao] = avaliacao;
                 return true;
             }
@@ -57,16 +63,16 @@ public class Resultado {
         TratamentoErro.alerta("Não foi possível adicionar avaliação, pois o vetor esta cheio.");
         return false;// Caso o array de avaliacao estiver cheio, então retorna false.
     }
-
+    //Método privado para retorna a posicao vazia do array
     private int verificaPosicaoDeInsercao(){
         for(int i = 0; i < this.avaliacao.length; i++){
             if(this.avaliacao[i] == null){
-                return i;// Retorna qual a posição do vetor esta livre
+                return i;// Retorna qual a posição do array esta livre
             }
         }
-        return -1;// Retorna valor coringa caso vetor esteja cheio.
+        return -1;// Retorna valor coringa caso o array esteja cheio.
     }            
-    
+    //Método privado para verificar se a avaliacao a ser adicionada não possui a mesma medida de avaliação de alguma avaliacao ja armazenada.
     private boolean verificaMedidaAvaliacaoIgual(ValorAvaliacao avaliacao){
         for(int i = 0; i < this.avaliacao.length; i++){//Laço de repetição para percorrer as avaliacões.
             if(Validador.verificaObjetoValido(this.avaliacao[i]) &&
@@ -76,11 +82,11 @@ public class Resultado {
         }
         return false;
     }
-
+    //Método que retorna o array de avaliações
     public ValorAvaliacao[] getAvaliacoes(){
     	return this.avaliacao;
     }
-
+    //Método que retorna uma determinada avaliação passada por parâmetro
     public ValorAvaliacao getAvaliacaoPorMedida(String medida){
     	for(int i = 0; i < this.avaliacao.length; i++){// Laço para percorrer o vetor de avaliações.
             if(Validador.verificaObjetoValido(this.avaliacao[i]) && // Verifica se o objeto é valido e Verificar se existe a medida, caso exista retorna a avaliação.
@@ -88,12 +94,11 @@ public class Resultado {
                 return avaliacao[i];// Retorna a avaliação.
             }
         }
-
-        TratamentoErro.alerta("Nenhuma avaliação foi encontrada com a medida " + medida);
+        
+        TratamentoErro.alerta("Nenhuma avaliação foi encontrada com a medida " + medida);//Método da classe TratamentoErro para exibir um alerta de que a avaliação não foi encontrada.
         return null;
     }
-    
-    //verifica se um objeto Resultado é igual ao objeto instanciado
+    //Método para comparar todos os objetos desta classe Resultado  
     public boolean equals(Resultado resultado){
 
         if(resultado.getAlgoritmo().equals(this.algoritmo) && resultado.getDataset().equals(this.dataset)){//compara os algoritmos e os datasets
