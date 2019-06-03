6eeadd2ae01e0244778f16e91e7c51bacb5b8dae
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2019-06-03 09:09:53 -0400

    Refatorando classe ValorAvaliacao

--- src/app/ValorAvaliacao.java
+++ src/app/ValorAvaliacao.java
@@ -6,17 +6,35 @@ public class ValorAvaliacao {
     private float valor;
     private MedidaAvaliacao medida;
 
+    // Método construtor responsável por invocar os métodos responsáveis pela validação e 
+    // inicialização dos atributos
     public ValorAvaliacao(MedidaAvaliacao medida){
-        // Verifica se a medida não é nula
-        if(Validador.verificaObjetoValido(medida))
-            this.medida = medida;
-        else
-            TratamentoErro.erro("O objeto informado não pode ser nulo.");
+        this.verificaMedidaAvaliacaoValida(medida); // Invoca método responsável por verificar se a medida de avaliação é nula
+        this.inicializaMedidaAvaliacao(medida); // Invoca método responsável por inicializar a medida de avaliação
+        
+        this.inicializaValor(); // Invoca método responsável por inicializar o atributo valor
+    }
 
-        // Inicializando com valor default: 0.0
+    // Método responsável por validar a medida de avaliação
+    private void verificaMedidaAvaliacaoValida(MedidaAvaliacao medida) {
+        // Se a medidade avaliação for nula o programa irá tratar o erro por meio
+        // da invocação de outro método
+        if (!Validador.verificaObjetoValido(medida)) {
+            TratamentoErro.erro("O objeto informado não pode ser nulo.");
+        }
+    }
+
+    // Método responsável por inicializar o atributo medida
+    private void inicializaMedidaAvaliacao(MedidaAvaliacao medida) {
+        this.medida = medida;
+    }
+
+    // Método responsável por inicializar o atributo valor
+    private void inicializaValor() {
         this.valor = 0.0f;
     }
 
+    // Método responsável por atribuir um valor ao atributo valor
     public boolean setValor(float valor){
         float menorValor = this.medida.getMenorValor(), 
               maiorValor = this.medida.getMaiorValor();
@@ -32,14 +50,17 @@ public class ValorAvaliacao {
         }
     }
 
+    // Método responsável por retornar a medida de avaliação
     public MedidaAvaliacao getMedida(){
         return this.medida;
     }
 
+    // Método responsável por retornar o valor
     public float getValor(){
         return this.valor;
     }
     
+    // Método responsável por verificar se dois objetos da classe ValorAvaliacao são iguais ou não
     public boolean equals(ValorAvaliacao valorAvaliacao){
         if(valorAvaliacao.getValor() == this.valor && valorAvaliacao.getMedida().equals(this.medida)){
             return true;
