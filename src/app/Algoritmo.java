8f11d4516e5e8d97502efa4f51ee59dc4fd175f0
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2019-06-02 20:03:44 -0400

    Refatorando classe Algoritmo

--- src/app/Algoritmo.java
+++ src/app/Algoritmo.java
@@ -6,33 +6,57 @@ public class Algoritmo {
     private String nome;
     private float[] parametros;
 
+    // Método construtor responsável por invocar os métodos responsáveis pela validação e 
+    // inicialização dos atributos
     public Algoritmo(String nome, float[] parametros){
-        // Verifica se o nome do algoritmo é válido
-        if(Validador.verificaStringValida(nome)) {
-            this.nome = nome;
-        } else {
+        this.verificaNomeValido(nome);// Verificando se o atributo nome é uma String válida
+        this.inicializaNome(nome); // Inicializando atributo nome
+
+        this.verificaParametrosValido(parametros); // Verificando se os parâmetros são válidos
+        this.inicializaParametros(parametros); // Inicializando atributo parametros
+    }
+
+    // Método responsável por validar o nome
+    private void verificaNomeValido(String nome) {
+        // Se o nome não for válido, o programa irá tratar o erro por meio da invocação de outro
+        // método
+        if(!Validador.verificaStringValida(nome)) {
             TratamentoErro.erro("Nome do algoritmo inválido: " + nome);
         }
+    }
 
-        // Verifica se a quantidade de parâmetros é valida
-        if(parametros.length == 3) {
-            this.parametros = parametros.clone();
-        } else {
+    // Método responsável por inicializar o atributo nome
+    private void inicializaNome(String nome) {
+        this.nome = nome;
+    }
+
+    // Método responsável por validar a quantidade de parâmetros
+    private void verificaParametrosValido(float[] parametros) {
+        // Se a quantidade de parâmetros for diferente de 3, o programa irá tratar o erro por meio
+        // da invocação de outro método
+        if(parametros.length != 3) {
             TratamentoErro.erro(parametros.length + " foram passados. "
                 + "3 parametros eram esperados.");
         }
     }
 
+    // Método responsável por inicializar a quantidade de parâmetros
+    private void inicializaParametros(float[] parametros) {
+        this.parametros = parametros.clone();
+    }
+
+    // Método responsável por retornar o nome do objeto algoritmo
     public String getNome(){
         return this.nome;
     }
 
+    // Método responsável por retornar o endereço de memória do atributo parâmetros
     public float[] getParametros(){
         return this.parametros;
     }
 
+    // Método equals, responsável por comparar se dois objetos são idênticos ou não
     public boolean equals(Algoritmo algoritmo){
-
         if(algoritmo.getNome() == this.nome){
             int parametrosIguais=0;
             for(int i=0; i < 3; i++){
